a778a56674a1fc217d2802a411101b706957a500
Signor_P <124287199+SignorMassimo@users.noreply.github.com>
2026-04-02 20:46:20 +0400

    Release 0.0.1 sync

---
+++ index.example.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import userRouter from './routes/user.route';
+
+const app = express()
+/* ... other middlewares or imports */
+app.use(userRouter)
+
+// app.listen(3000, () => console.log('Server listening on 3000 port'))
